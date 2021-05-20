80ccf2c03b451d59743cb445836406f559be9b58
Jason <jasonpsmith180@gmail.com>
2021-05-19 20:59:54 -0600

    trouble deleting replies

--- models/Comment.js
+++ models/Comment.js
@@ -1,16 +1,62 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema ({
-    writtenBy: {
-        type: String
-    }, 
-    commentBody: {
-        type: String
+const ReplyScehma = new Schema(
+    {
+        // set custom id to avoid confustion with parent comment_id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String,
+            required: true
+        },
+        writtenBy: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            defualt: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
     },
-    createdAt: {
-        type: Date,
-        default: Date.now
+    {
+        toJSON: {
+            getters: true
+        }
     }
+);
+
+const CommentSchema = new Schema (
+    {
+        writtenBy: {
+            type: String,
+            required: true
+        }, 
+        commentBody: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        replies: [ReplyScehma]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
